5833dd009cdb92ad89b17a865fae8aeb3fc2d86f
maliwahn <lucky777men@mail.ru>
2023-09-30 04:13:31 +0300

    1 commit

--- src/main/java/ru/yandex/practicum/filmorate/storage/ValidationManager.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/ValidationManager.java
@@ -1,7 +1,7 @@
 package ru.yandex.practicum.filmorate.storage;
 
 import ru.yandex.practicum.filmorate.exception.ValidationException;
-import ru.yandex.practicum.filmorate.model.User;
+
 
 import java.time.LocalDate;
 
